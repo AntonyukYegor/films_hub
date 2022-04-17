90795a1215fbf47356c6ba99b25985dc43885947
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-04-17 18:56:00 +0400

    Add AbstractFilm model

---
+++ lib/app/models/films/abstract_film.dart
@@ -0,0 +1,17 @@
+abstract class AbstractFilm {
+  String get id;
+
+  String get title;
+
+  String get poster;
+
+  double get voteAverage;
+
+  String get releaseDate;
+
+  String get description;
+
+  String get duration;
+
+  String get language;
+}
