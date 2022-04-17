54470d34a34c1ec44957d611f4bb9fa60b543b05
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-04-17 22:09:19 +0400

    Add empty named constructor for BaseFilter

--- lib/app/models/filters/base_filter.dart
+++ lib/app/models/filters/base_filter.dart
@@ -3,6 +3,8 @@ import 'package:films_hub/app/models/filters/abstract_filter.dart';
 class BaseFilter<T> implements AbstractFilter<T> {
   final Iterable<AbstractFilter<T>> _filterChainLinks;
 
+  BaseFilter.empty(): this.chain([]);
+
   BaseFilter(AbstractFilter<T> filter) : this.chain([filter]);
 
   BaseFilter.chain(this._filterChainLinks);
