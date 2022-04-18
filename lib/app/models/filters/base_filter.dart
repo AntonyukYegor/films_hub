00d5bec76408f5fb99aa5abdb4c4fb726a8bd664
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-04-18 17:58:04 +0400

    Changed the algorithm for applying sequential filtering from recursion to a loop to prevent stack overflow

--- lib/app/models/filters/base_filter.dart
+++ lib/app/models/filters/base_filter.dart
@@ -3,7 +3,7 @@ import 'package:films_hub/app/models/filters/abstract_filter.dart';
 class BaseFilter<T> implements AbstractFilter<T> {
   final Iterable<AbstractFilter<T>> _filterChainLinks;
 
-  BaseFilter.empty(): this.chain([]);
+  BaseFilter.empty() : this.chain([]);
 
   BaseFilter(AbstractFilter<T> filter) : this.chain([filter]);
 
@@ -11,13 +11,12 @@ class BaseFilter<T> implements AbstractFilter<T> {
 
   @override
   Stream<T> apply(Stream<T> source) async* {
-    yield* _filterFunc(source, _filterChainLinks);
-  }
+    var result = source;
 
-  Stream<T> _filterFunc(
-      Stream<T> source, Iterable<AbstractFilter<T>> filters) async* {
-    yield* (filters.isEmpty)
-        ? source
-        : _filterFunc(filters.first.apply(source), filters.skip(1));
+    for (var filter in _filterChainLinks) {
+      result = filter.apply(result);
+    }
+
+    yield* result;
   }
 }
