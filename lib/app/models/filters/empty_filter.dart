fbf86039061c2c4ee20e10ecb53126cf756abc46
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-04-26 17:49:39 +0400

    Delete unused class

--- lib/app/models/filters/empty_filter.dart
+++
@@ -1,8 +0,0 @@
-import 'package:films_hub/app/models/filters/abstract_filter.dart';
-
-class EmptyFilter<T> implements AbstractFilter<T> {
-  @override
-  T apply(T source) {
-    return source;
-  }
-}
