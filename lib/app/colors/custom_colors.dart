ab89f71419498d3f439f9755a127deddd22b34bf
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-04-28 15:05:43 +0400

    Moved darkThemeMaterialColor to self file

---
+++ lib/app/colors/custom_colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomColors implements Colors {
+  static const MaterialColor _darkBlack = MaterialColor(
+    _darkBlackPrimaryValue,
+    <int, Color>{
+      50: Color(0x00000000),
+      100: Color(0x05050505),
+      200: Color(0x10101010),
+      300: Color(0xA0A0A0A0),
+      400: Color(0xB0B0B0B0),
+      500: Color(_darkBlackPrimaryValue),
+      600: Color(0xC0C0C0C0),
+      700: Color(0xD0D0D0D0),
+      800: Color(0xF0F0F0F0),
+      900: Color(0xFFFFFFFF),
+    },
+  );
+
+  static MaterialColor get darkBlack => _darkBlack;
+
+  static const int _darkBlackPrimaryValue = 0xA0FFFFfF;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:films_hub/app/behaviors/custom_scroll_behavior.dart';
+import 'package:films_hub/app/colors/custom_colors.dart';
 import 'package:films_hub/app/pages/home_page.dart';
 import 'package:films_hub/app/repositories/films/extended_fake_films_repository.dart';
 import 'package:flutter/material.dart';
@@ -9,22 +10,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  static const MaterialColor _darkThemeColor = MaterialColor(
-    _bluePrimaryValue,
-    <int, Color>{
-      50: Color(0x00000000),
-      100: Color(0x05050505),
-      200: Color(0x10101010),
-      300: Color(0xA0A0A0A0),
-      400: Color(0xB0B0B0B0),
-      500: Color(_bluePrimaryValue),
-      600: Color(0xC0C0C0C0),
-      700: Color(0xD0D0D0D0),
-      800: Color(0xF0F0F0F0),
-      900: Color(0xFFFFFFFF),
-    },
-  );
-  static const int _bluePrimaryValue = 0xA0FFFFfF;
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +26,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.system,
       darkTheme: ThemeData(
           brightness: Brightness.dark,
-          primarySwatch: _darkThemeColor,
+          primarySwatch: CustomColors.darkBlack,
           fontFamily: fontFamily,
           shadowColor: Colors.white.withOpacity(0.08),
           scaffoldBackgroundColor: Colors.black),
@@ -51,7 +36,10 @@ class MyApp extends StatelessWidget {
         shadowColor: Colors.black.withOpacity(0.3),
         fontFamily: fontFamily,
       ),
-      home: HomePage(ExtendedFakeFilmsRepository.delayedInMilliseconds(100),title: 'Movies List'),
+      home: HomePage(
+        ExtendedFakeFilmsRepository.delayedInMilliseconds(100),
+        title: 'Movies List',
+      ),
     );
   }
 }
