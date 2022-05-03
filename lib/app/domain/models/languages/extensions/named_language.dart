59de8e23b06864c6f9f4aad7e30a2d6314291438
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-05-03 23:44:39 +0400

    Add Spanish language

--- lib/app/domain/models/languages/extensions/named_language.dart
+++ lib/app/domain/models/languages/extensions/named_language.dart
@@ -9,6 +9,8 @@ extension NamedLanguage on Language {
         return 'English';
       case Language.french:
         return 'French';
+      case Language.spanish:
+        return 'Spanish';
       case Language.unknown:
         return 'Unknown';
     }
@@ -22,8 +24,42 @@ extension NamedLanguage on Language {
         return 'ENG';
       case Language.french:
         return 'FR';
+      case Language.spanish:
+        return 'ES';
       case Language.unknown:
         return 'UNK';
+    }
+  }
+}
+
+extension LanguageString on String {
+  Language fromPrettyString() {
+    switch (this) {
+      case 'Russian':
+        return Language.russian;
+      case 'English':
+        return Language.english ;
+      case 'French':
+        return Language.french ;
+      case 'Spanish':
+        return Language.spanish ;
+      default :
+        return Language.unknown;
+    }
+  }
+
+  Language fromShortString() {
+    switch (this) {
+      case 'RU':
+        return Language.russian;
+      case 'ENG':
+        return Language.english ;
+      case 'FR':
+        return Language.french ;
+      case 'ES':
+        return Language.spanish;
+      default :
+        return Language.unknown;
     }
   }
 }
