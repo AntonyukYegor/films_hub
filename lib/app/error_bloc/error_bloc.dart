e30fddd81823047a0060270d0334f5238c8b4355
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-05-06 03:37:35 +0400

    Added error bloc

---
+++ lib/app/error_bloc/error_bloc.dart
@@ -0,0 +1,17 @@
+import 'package:films_hub/app/components/dialogs/error_dialog.dart';
+import 'package:films_hub/app/error_bloc/error_event.dart';
+import 'package:films_hub/app/error_bloc/error_state.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ErrorBloc extends Bloc<ErrorEvent, ErrorState> {
+  final BuildContext _context;
+
+  ErrorBloc(this._context) : super(const ErrorState()) {
+    on<ShowDialogEvent>(_onShowDialog);
+  }
+
+  void _onShowDialog(ShowDialogEvent event, Emitter<ErrorState> emit) {
+    showErrorDialog(_context, error: '${event.title} ${event.message}');
+  }
+}
