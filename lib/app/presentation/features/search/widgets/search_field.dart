de696519ee88105d4b1296026a4a3f83b14e6e07
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-05-12 16:31:19 +0400

    Removed unused widget

--- lib/app/presentation/features/search/widgets/search_field.dart
+++ lib/app/presentation/features/search/widgets/search_field.dart
@@ -34,41 +34,38 @@ class SearchFieldState extends State<SearchField> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // padding: const EdgeInsets.only(left: 16, top: 32, right: 16),
-      child: Stack(
-        children: [
-          const Positioned.fill(
-              child: AppThemeCardBackgroundWithoutShadow(
-                  AppStyle.randomBackgroundImageUrl, 16, 0)),
-          ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(16)),
-            child: Theme(
-              data: Theme.of(context).copyWith(
-                  focusColor: Theme.of(context).textTheme.titleSmall?.color),
-              child: TextField(
-                controller: _textController,
-                maxLines: 1,
-                autofocus: false,
-                cursorColor: Theme.of(context).textTheme.titleSmall?.color,
-                decoration: InputDecoration(
-                  focusColor: Theme.of(context).textTheme.titleSmall?.color,
-                  hoverColor: Theme.of(context).textTheme.titleSmall?.color,
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  labelText: 'Search',
-                  labelStyle: TextStyle(
-                    color: Theme.of(context).textTheme.titleSmall?.color,
-                  ),
-                  filled: true,
-                  fillColor: Colors.transparent,
+    return Stack(
+      children: [
+        const Positioned.fill(
+            child: AppThemeCardBackgroundWithoutShadow(
+                AppStyle.randomBackgroundImageUrl, 16, 0)),
+        ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          child: Theme(
+            data: Theme.of(context).copyWith(
+                focusColor: Theme.of(context).textTheme.titleSmall?.color),
+            child: TextField(
+              controller: _textController,
+              maxLines: 1,
+              autofocus: false,
+              cursorColor: Theme.of(context).textTheme.titleSmall?.color,
+              decoration: InputDecoration(
+                focusColor: Theme.of(context).textTheme.titleSmall?.color,
+                hoverColor: Theme.of(context).textTheme.titleSmall?.color,
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                labelText: 'Search',
+                labelStyle: TextStyle(
+                  color: Theme.of(context).textTheme.titleSmall?.color,
                 ),
-                onChanged: widget._onSearchFieldTextChanged,
+                filled: true,
+                fillColor: Colors.transparent,
               ),
+              onChanged: widget._onSearchFieldTextChanged,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
