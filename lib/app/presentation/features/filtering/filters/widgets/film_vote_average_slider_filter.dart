5392f1a4d51a0029575901e1a54105a8cd866a07
AntonyukYegor <86590132+AntonyukYegor@users.noreply.github.com>
2022-05-18 12:59:34 +0400

    fixed pattern filter vote averages

--- lib/app/presentation/features/filtering/filters/widgets/film_vote_average_slider_filter.dart
+++ lib/app/presentation/features/filtering/filters/widgets/film_vote_average_slider_filter.dart
@@ -1,4 +1,3 @@
-import 'package:films_hub/app/components/delayed_action.dart';
 import 'package:films_hub/app/domain/models/films/abstract_film.dart';
 import 'package:films_hub/app/domain/models/filters/abstract_filter.dart';
 import 'package:films_hub/app/domain/models/filters/conditions/films/film_clamp_by_vote_average_condition.dart';
@@ -6,6 +5,7 @@ import 'package:films_hub/app/domain/models/filters/film_filter_source.dart';
 import 'package:films_hub/app/domain/models/filters/films/film_future_list_filter.dart';
 import 'package:films_hub/app/presentation/features/filtering/filters/bloc/filters_bloc.dart';
 import 'package:films_hub/app/presentation/features/filtering/filters/bloc/filters_event.dart';
+import 'package:films_hub/app/presentation/features/filtering/filters/bloc/filters_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,59 +19,48 @@ class FilmVoteAverageSliderFilter extends StatefulWidget {
 
 class FilmVoteAverageSliderFilterState
     extends State<FilmVoteAverageSliderFilter> implements FilmFilterSource {
-  RangeValues _currentRangeValues = _defaultRangeValues;
   static const RangeValues _defaultRangeValues = RangeValues(0, 10);
 
   @override
-  void initState() {
-    super.initState();
-    _currentRangeValues = context.read<FiltersBloc>().state.voteAverageRange;
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return RangeSlider(
-      min: 0.0,
-      max: 10.0,
-      divisions: 100,
-      labels: RangeLabels(
-        _currentRangeValues.start.toStringAsFixed(1).toString(),
-        _currentRangeValues.end.toStringAsFixed(1).toString(),
-      ),
-      onChanged: (RangeValues values) {
-        setState(() {
-          _currentRangeValues = values;
-        });
-        DelayedAction.run(() {
+    return BlocBuilder<FiltersBloc, FiltersState>(builder: (context, state) {
+      final currentRange = context.read<FiltersBloc>().state.voteAverageRange;
+      return RangeSlider(
+        min: _defaultRangeValues.start.toDouble(),
+        max: _defaultRangeValues.end.toDouble(),
+        divisions: 100,
+        labels: RangeLabels(
+          currentRange.start.toStringAsFixed(1).toString(),
+          currentRange.end.toStringAsFixed(1).toString(),
+        ),
+        onChanged: (RangeValues values) {
           context
               .read<FiltersBloc>()
-              .add(ChangeVoteAverageRangeEvent(range: _currentRangeValues));
-        });
-      },
-      values: _currentRangeValues,
-    );
+              .add(ChangeVoteAverageRangeEvent(range: values));
+        },
+        values: currentRange,
+      );
+    });
   }
 
   @override
   void reset() {
-    setState(() {
-      _currentRangeValues = _defaultRangeValues;
-    });
     context
         .read<FiltersBloc>()
-        .add(ChangeVoteAverageRangeEvent(range: _currentRangeValues));
+        .add(const ChangeVoteAverageRangeEvent(range: _defaultRangeValues));
   }
 
   @override
   AbstractFilter<Future<List<AbstractFilm>>> filter() {
-    if (_currentRangeValues == _defaultRangeValues) {
+    final currentRange = context.read<FiltersBloc>().state.voteAverageRange;
+    if (currentRange == _defaultRangeValues) {
       return FilmFutureListFilter.empty();
     }
 
     return FilmFutureListFilter.condition(
       FilmClampByVoteAverageCondition(
-        _currentRangeValues.start,
-        _currentRangeValues.end,
+        currentRange.start,
+        currentRange.end,
       ),
     );
   }
